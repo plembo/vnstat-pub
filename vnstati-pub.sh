ad8faa62964e348fead16837114f3471deef139c
Phil Lembo <phil.lembo@outlook.com>
2022-07-18 15:20:27 -0400

    Revise script to create and push reports

--- vnstati-pub.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# To copy vnstat reports to web server
-keyfile=/root/.ssh/id_rsa
-pubdir=/data1/www/html/vnstati
-remusr=admin@gateway.example.com
-srcdir=/tmp
-
-echo "Get vnstat reports"
-scp -i $keyfile -s $remusr:$srcdir/vnstat-*.png $pubdir
--- vnstati-update.sh
+++ vnstati-update.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+# Script in $HOME/bin on router to create report files and copy to web server
+HOME="/home/admin"
+srcdir="$HOME/vnstat"
+keyfile="$HOME/.ssh/id_rsa"
+remusr="root@jupiter.casalembo.com"
+pubdir="d1/www/html/vnstati"
+
+echo "Create vnstat reports"
 /usr/bin/vnstati -i eth0 -s -o /tmp/vnstat-summary.png
 /usr/bin/vnstati -i eth0 -m -o /tmp/vnstat-monthly.png
 /usr/bin/vnstati -i eth0 -d -o /tmp/vnstat-daily.png
 /usr/bin/vnstati -i eth0 -h -o /tmp/vnstat-hourly.png
+
+echo "Copy reports to web server"
+/usr/bin/scp -i $keyfile $srcdir/vnstat-*.png $remusr:/$pubdir
