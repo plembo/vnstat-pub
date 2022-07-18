4077fca719fb684b513d7d493556a0ca118c5703
Phil Lembo <phil.lembo@outlook.com>
2022-07-18 17:47:18 -0400

    Set file create path to

--- vnstati-update.sh
+++ vnstati-update.sh
@@ -7,10 +7,10 @@ remusr="root@jupiter.casalembo.com"
 pubdir="d1/www/html/vnstati"
 
 echo "Create vnstat reports"
-/usr/bin/vnstati -i eth0 -s -o /tmp/vnstat-summary.png
-/usr/bin/vnstati -i eth0 -m -o /tmp/vnstat-monthly.png
-/usr/bin/vnstati -i eth0 -d -o /tmp/vnstat-daily.png
-/usr/bin/vnstati -i eth0 -h -o /tmp/vnstat-hourly.png
+/usr/bin/vnstati -i eth0 -s -o $srcdir/vnstat-summary.png
+/usr/bin/vnstati -i eth0 -m -o $srcdir/vnstat-monthly.png
+/usr/bin/vnstati -i eth0 -d -o $srcdir/vnstat-daily.png
+/usr/bin/vnstati -i eth0 -h -o $srcdir/vnstat-hourly.png
 
 echo "Copy reports to web server"
 /usr/bin/scp -i $keyfile $srcdir/vnstat-*.png $remusr:/$pubdir
